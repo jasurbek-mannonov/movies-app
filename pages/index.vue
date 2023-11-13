e7db69116cfdd4b1f958353c727a619993332d74
jasurbek-mannonov <jasurbekmannonov@gmail.com>
2023-11-14 00:13:49 +0500

    some changes around searchBar

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,18 @@
 <script setup>
+const searchError = ref(false);
+const searchMovie = (searchInput) => {
+    // console.log("Search Input: ", searchInput)
+    if(searchInput === ""){
+        searchError.value = true;
+    }
+    if(searchInput !== ""){
+        searchError.value = false;
+    }
+};
 </script>
 
 <template>
     <div>
-        <SearchBar/> 
+        <SearchBar @search-movie="searchMovie" :searchError="searchError"/> 
     </div>
 </template>
