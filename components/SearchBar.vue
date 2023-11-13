99ba5d6d243a41b022d62f32470c849d97db2273
jasurbek-mannonov <jasurbekmannonov@gmail.com>
2023-11-14 00:00:01 +0500

    Completed searchBar

---
+++ components/SearchBar.vue
@@ -0,0 +1,18 @@
+<script setup>
+const searchInput = ref("")
+</script>
+
+<template>
+    <section class="search-section bg-cyan-50 p-4">
+        <div class="container mx-auto md:px-40 px-5">
+            <div class="flex">
+                <input class="border border-cyan-400 py-2 px-4 w-full rounded-l-full focus:outline-none focus:ring-2 focus:ring-cyan-400" type="text"
+                placeholder="Search Movie..." 
+                v-model.lazy="searchInput"/>
+                <button class="bg-cyan-500 hover:bg-cyan-600 text-white py-2 px-4 rounded-r-full" type="submit">
+                    Search
+                </button>
+            </div>
+        </div>
+    </section>
+</template>
