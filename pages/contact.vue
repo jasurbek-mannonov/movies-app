6c33a84102362c66250ac30fe3a32670b18e0882
jasurbek-mannonov <jasurbekmannonov@gmail.com>
2023-11-13 23:47:32 +0500

    changes

---
+++ pages/contact.vue
@@ -0,0 +1,30 @@
+<script setup>
+</script>
+
+<template>
+    <div class="flex flex-col md:flex-row">
+        <div class="md:w-1/2 p-8">
+            <img src="https://cdn.pixabay.com/photo/2015/11/03/08/58/meeting-1019875_960_720.jpg" alt="Contact Us"/>
+        </div>
+        <div class="md:w-1/2 p-8 shadow-md">
+            <p class="text-lg mb-2">
+                7 Buyuk Kelajak street <br />
+                Gulistan, Sirdaryo 301100
+            </p>
+            <p class="text-lg mb-2">
+                Phone: +998 90 241 68 57 <br />
+                Email: jasurbekmannonov2020@gmail.com
+            </p>
+            <form>
+                <label for="name" class="block text-lg font-bold mb-2">Name</label>
+                <input type="text" id="name" class="w-full border border-gray-300 p-2 mb-4 rounded-lg"/>
+                <label for="email" class="block text-lg font-bold mb-2">Email</label>
+                <input type="text" id="email" class="w-full border border-gray-300 p-2 mb-4 rounded-lg"/>
+
+                <label for="name" class="block text-lg font-bold mb-2">Message</label>
+                <textarea name="message" id="message" class="w-full border border-gray-300 p-2 mb-4 rounded-lg"></textarea>
+                <button type="submit" class="bg-rose-500 hover:bg-rose-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Submit</button>
+            </form>
+        </div>
+    </div>
+</template>
---
+++ pages/registration.vue
@@ -0,0 +1,40 @@
+<script setup>
+</script>
+
+<template>
+    <div class="w-full max-w-md mx-auto mt-5">
+        <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <div class="mb-4">
+                <label class="block text-gray-700 font-bold mb-2" for="username">
+                    Username
+                </label>
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" id="username" placeholder="Username"/>
+            </div>
+
+            <div class="mb-4">
+                <label class="block text-gray-700 font-bold mb-2" for="email">
+                    Email
+                </label>
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="email" id="email" placeholder="Email"/>
+            </div>
+
+            <div class="mb-4">
+                <label block text-gray-700 font-bold mb-2 for="password">
+                    Password
+                </label>
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="password" id="password" placeholder="Password"/>
+            </div>
+            <div class="mb-4">
+                <label block text-gray-700 font-bold mb-2 for="rePassword">
+                  Repeat password
+                </label>
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="password" id="rePassword" placeholder="Repeat password"/>
+            </div>
+            <div class="flex items-center justify-center mt-6">
+                <button class="bg-rose-500 hover:bg-rose-700 text-white font-bold py-2 px-8 rounded-full focus:outline-none focus:shadow-outline" type="button">
+                    Sign Up
+                </button>
+            </div>
+        </form>
+    </div>
+</template>
